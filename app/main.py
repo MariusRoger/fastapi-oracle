bd643c6fe1bdcd0ec147d47847fb78e4de2418d3
Marius Roger <122625850+MariusRoger@users.noreply.github.com>
2024-12-03 16:11:36 +0100

    feat: add listing of test database through api

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
+from typing import Annotated
 
-from app import database
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from app import database, database_models
 
 app = FastAPI()
 
 
 @app.get("/")
-async def get_root_with_db():
-    return {"response": "Hello World !"}
+async def get_root_with_db(db: Annotated[Session, Depends(database.get_db)]):
+
+    all_results = db.query(database_models.TestTable).all()
+
+    return all_results
